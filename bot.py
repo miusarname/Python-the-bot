b5f57248c9c047392001df13aca3e4071903e950
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-02-10 23:24:55 -0500

    Se creo comando de preguntas random (Rapidas)

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 
 client = commands.Bot(command_prefix='/',intents=discord.Intents.all())
@@ -13,4 +14,13 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 
-client.run('MTA3MzYwODA1NDE2MTAxNDg0NQ.G0t0Il.Vh0oK00-cjSnKTqYf4g76cciZpG-looeHbVfp4')
+@client.command(aliases=["RandomQ","Split Question","QR"])
+async def question_ramdom(ctx,*,starComand):
+    with open('example_question.txt','r')as f:
+        random_response =f.readlines()
+        response=random.choice(random_response)
+
+    await ctx.send(response)
+        
+
+client.run('MTA3MzYwODA1NDE2MTAxNDg0NQ.GydovL.vompnPYjvunYN3-lzqmuo-x6h8TKDp3wu1sWsk')
